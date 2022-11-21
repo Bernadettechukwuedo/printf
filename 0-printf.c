c524526b8ef08633a2211c5341f07d48fd75fa4a
Bernadette <chukwuedoogochukwubernadette@gmail.com>
2022-11-21 08:39:28 -0800

    function that produces output according to a format

---
+++ 0-printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _printf - a function that produces output according to a format
+ * @format: is a character printf
+ * a function pointer, that accepts va_list as argument
+ * Declare a variable list, with its own argument(begins at va_start)
+ * Return - the total number of characters printed(count)
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int j = 0, count = 0;
+
+	int (*ptr_func)(va_list);
+
+	/* Returns -1 if format is null */
+
+	if (!format || (format[j] == '%' && format[j + 1] == '\0'))
+		return (-1);
+	if (!format[j])
+		return (0);
+
+	va_start(list, format);
+	for (j = 0; format[j] != '\0'; j++)
+	{
+		if (format[j] == '%')
+		{
+			if (format[j + 1] == '\0')
+				return (-1);
+
+			ptr_func = get_func(format, j + 1);
+			if (ptr_func == NULL)
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				count += ptr_func(list);
+				j += get_nflags(format, j + 1);
+			}
+		}
+		else
+		{
+			_putchar(format[j]);
+			count++;
+		}
+	}
+	return (count);
+}
--- main.h
+++ main.h
@@ -1,6 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+
+/**
+ * struct print - Struct op
+ *
+ * @c: placeholder the character or format specifier
+ * @func: function that prints data with the specifier
+ */
+typedef struct print
+{
+	/* char *c: formats c, s, b, i, d, o, x, ... */
+	char *c;
+	/* A function that collects the variable list data and prints with it */
+	int (*func)(va_list);
+} prn;
 
 int _printf(const char *format, ...);
+int _putchar(char);
+
+/* returns a pointer to the function to execute using structs */
+int (*get_func(const char *, int))(va_list);
+int get_nflags(const char *, int);
 
 #endif
